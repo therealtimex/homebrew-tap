aedd7beee77d06b0afcf4a45833d616f650e8d81
rta_phuongnguyen <phuongnguyen@rtanalytics.vn>
2026-01-22 16:01:32 +0700

    add version

---
+++ Casks/realtimex@1.0.59-dev.rb
@@ -0,0 +1,25 @@
+cask "realtimex" do
+  # Define what 'arch' should resolve to for each CPU
+  arch arm: "-arm64", intel: ""
+
+  version "1.0.59-dev"
+  
+  # Provide both SHA256 hashes
+  sha256 arm:   "9c7efc95d2aee25d9830ffe36b53c6ee7a1f5e9881ac0238ade3f373eb2585ed",
+         intel: "d490fd140318ffd1be51301a87720366f2eaf78e515f150e859f20b1ae1c5eb5"
+
+  # Use the #{arch} variable in your URL
+  url "https://github.com/therealtimex/realtimex/releases/download/v#{version}/RealTimeX.AI-#{version}#{arch}.dmg"
+  
+  name "RealTimeX"
+  desc "Find powerful AI Agents for RealTimeX"
+  homepage "https://realtimex.ai/"
+
+  depends_on formula: "redis"
+  app "RealTimeX.AI.app"
+
+  # Optional: What to tell the user after installation
+  caveats <<~EOS
+    RealTimeX.AI will be placed in Applications.
+  EOS
+end
---
+++ Casks/realtimex@1.0.60.rb
@@ -0,0 +1,25 @@
+cask "realtimex" do
+  # Define what 'arch' should resolve to for each CPU
+  arch arm: "-arm64", intel: ""
+
+  version "1.0.60"
+  
+  # Provide both SHA256 hashes
+  sha256 arm:   "30a7e382ab579fea12feaf7cd405f3d32bcb08a707de575a33b63570cfa69055",
+         intel: "93e789c53ba141f5f62dab905ac27f9605fca6be7f00062f45009bf103925378"
+
+  # Use the #{arch} variable in your URL
+  url "https://github.com/therealtimex/realtimex/releases/download/v#{version}/RealTimeX.AI-#{version}#{arch}.dmg"
+  
+  name "RealTimeX"
+  desc "Find powerful AI Agents for RealTimeX"
+  homepage "https://realtimex.ai/"
+
+  depends_on formula: "redis"
+  app "RealTimeX.AI.app"
+
+  # Optional: What to tell the user after installation
+  caveats <<~EOS
+    RealTimeX.AI will be placed in Applications.
+  EOS
+end
