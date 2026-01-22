75d84742362c6c7e6367ba269d6f46e3ec1614f6
rta_phuongnguyen <phuongnguyen@rtanalytics.vn>
2026-01-22 15:44:57 +0700

    update

--- Casks/realtimex.rb
+++ Casks/realtimex.rb
@@ -5,8 +5,8 @@ cask "realtimex" do
   version "1.0.60"
   
   # Provide both SHA256 hashes
-  sha256 arm:   "sha256:30a7e382ab579fea12feaf7cd405f3d32bcb08a707de575a33b63570cfa69055",
-         intel: "sha256:93e789c53ba141f5f62dab905ac27f9605fca6be7f00062f45009bf103925378"
+  sha256 arm:   "30a7e382ab579fea12feaf7cd405f3d32bcb08a707de575a33b63570cfa69055",
+         intel: "93e789c53ba141f5f62dab905ac27f9605fca6be7f00062f45009bf103925378"
 
   # Use the #{arch} variable in your URL
   url "https://github.com/therealtimex/realtimex/releases/download/v#{version}/RealTimeX.AI-#{version}#{arch}.dmg"
@@ -20,6 +20,6 @@ cask "realtimex" do
 
   # Optional: What to tell the user after installation
   caveats <<~EOS
-    RealTimeX.AI has installed in Applications. You can start using it now.
+    RealTimeX.AI will be placed in Applications.
   EOS
 end
